69b27f859f699d385cad95d69fd0956af597df1c
Tyrus Sonneborn <tyrus.b.sonneborn@gmail.com>
2018-07-11 18:08:04 -0400

    saved

--- ArcadeGameProject/src/game/MonsterHandler.java
+++ ArcadeGameProject/src/game/MonsterHandler.java
@@ -1,6 +1,11 @@
 package game;
 
+import java.awt.geom.Point2D;
+
 import deathcommands.DeathCommand;
+
 public interface MonsterHandler {
 	public void remove(DeathCommand command);
+
+	public void addMonster(int code, Point2D location);
 }
